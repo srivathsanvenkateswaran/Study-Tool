c2c10630c861f9885a459f22a7d3f697196bb3f4
gokul <gokulap@student.tce.edu>
2021-04-15 19:08:06 +0530

    deleted cache

--- compUsage.py
+++ compUsage.py
@@ -29,29 +29,29 @@ def clearRecycleBin():
 def sortAndstoreFiles(path):
 	import os
 	import shutil
-	
+
 	# This will create a properly organized
 	# list with all the filename that is
 	# there in the directory
 	list_ = os.listdir(path)
-	
+
 	# This will go through each and every file
 	for file_ in list_:
 		name, ext = os.path.splitext(file_)
-	
+
 		# This is going to store the extension type
 		ext = ext[1:]
-	
+
 		# This forces the next iteration,
 		# if it is the directory
 		if ext == '':
 			continue
-	
+
 		# This will move the file to the directory
 		# where the name 'ext' already exists
 		if os.path.exists(path+'/'+ext):
 		shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
-	
+
 		# This will create a new directory,
 		# if the directory does not already exist
 		else:
@@ -63,7 +63,7 @@ def shorten(url):
 	import contextlib
 	from urllib.parse import urlencode
 	from urllib.request import urlopen
-  	
+
   	#shortening url with tinyurl api
 	request_url = ('http://tinyurl.com/api-create.php?' + urlencode({'url':url}))
   	with contextlib.closing(urlopen(request_url)) as response:
@@ -74,6 +74,5 @@ def unshorten(url):
 
 	#returns the unshortened url
     return requests.head(url, allow_redirects=True).url
-
 
 
