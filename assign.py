7d84331ae093e031c3807278327ddaf1590a5212
gokul <gokulap@student.tce.edu>
2021-04-18 16:14:25 +0530

    added cropper function

--- assign.py
+++ assign.py
@@ -24,5 +24,77 @@ def TaskNotifier(site, time, task):
 
     #popup will open with task given at that time
 
+def cropper(path):
+	import cv2
+	import os
+	import sys
+	import shutil
+	from PIL import Image
+
+	if sys.platform == 'linux':
+		try:
+			os.system('mkdir cropped')
+		except:
+			shutil.rmtree(os.getcwd+"//cropped")
+	elif sys.platform == 'win32':
+		try:
+			os.system('md cropped')
+		except:
+			shutil.rmtree(os.getcwd+"//cropped")
+	else:
+		pass
+
+	refPt = []
+	cropping = False
+	def click_and_crop(event, x, y, flags, param):
+
+		global refPt, cropping
+
+		if event == cv2.EVENT_LBUTTONDOWN:
+			refPt = [(x, y)]
+			cropping = True
+		elif event == cv2.EVENT_LBUTTONUP:
+			refPt.append((x, y))
+			poi = open("co-ordinates.txt","w")
+			left,top = refPt[0]
+			right,bottom = refPt[1]
+			poi.write(str(refPt[0][1])+','+str(refPt[1][1])+','+str(refPt[0][0])+','+str(refPt[1][0]))
+			poi.close()
+			cropping = False
+			cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 3)
+			cv2.imshow("image", image)
+
+
+	image = cv2.imread(path)
+	clone = image.copy()
+	cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+	cv2.setMouseCallback("image", click_and_crop)
+
+	while True:
+
+		cv2.imshow("image", image)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("r"):
+			image = clone.copy()
+		elif key == ord("c"):
+			break
+
+	if len(refPt) == 2:
+		def filename(i = '1'):
+			arr = os.listdir(os.getcwd()+"//cropped")
+			if "{}.jpg".format(i) in arr:
+				i = int(i)
+				i = i + 1
+				filename(str(i))
+			else:
+				return i
+
+		name = str(filename())
+		roi = clone[refPt[0][1]:refPt[1][1],refPt[0][0]:refPt[1][0]]
+		cv2.imshow("ROI", roi)
+		cv2.imwrite(os.getcwd()+"//cropped//{}.jpg".format(name), roi)
+		cv2.waitKey(0)
+
+	cv2.destroyAllWindows()
 
 
