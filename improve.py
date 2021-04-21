4bd00a780897a78aca27fb93df15d4b6df7a979f
gokul <gokulap@student.tce.edu>
2021-04-21 20:28:35 +0530

    added safe eyes function

--- improve.py
+++ improve.py
@@ -6,4 +6,20 @@ next update:
 website blocker
 audiobook (need to find best module)
 book recommender
+
+pip install safeeyes
 '''
+
+def SafeEyes(command):
+	import os
+	if command == 'enable':
+		os.system('safeeyes &')
+	if command == 'disable':
+		os.system('safeeyes -d')
+	if command == 'settings'
+		os.system('safeeyes -s')
+	if commmand == 'status'
+		os.system('safeeyes --status')
+
+
+#### need to add
