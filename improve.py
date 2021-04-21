b353f62f734b37ec056ca702ede9d9f8943dda48
gokul <gokulap@student.tce.edu>
2021-04-21 19:53:47 +0530

    added next uppdate

--- improve.py
+++ improve.py
@@ -1,8 +1,9 @@
 '''
-audiobook
-book recommender
 website blocker
 Eye protection mode
 Audiobook from Youtube [Play audiobook from Youtube and play the Audio file]
 
+next update:
+audiobook (need to find best module)
+book recommender
 '''
