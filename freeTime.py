686844648622422540e00e7b67449210625348a6
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2021-05-03 22:55:35 +0530

    Commit

--- freeTime.py
+++ freeTime.py
@@ -125,80 +125,188 @@ def streamYoutubeVideos(videoURL):
     time.sleep(videoDuration)
     # wait till the video ends 
 
-class games:
+def rockPaperScissor(userOption, computerOption):
+    global userPoints, computerPoints
+    if(userOption==computerOption):
+        winner='Match Drawn'
+    else:
+        if(userOption=='Rock'):
+            if(computerOption=='Paper'):
+                winner='Computer Won'
+                computerPoints+=1
+            else:
+                winner='You Won'
+                userPoints+=1
+        elif(userOption=='Paper'):
+            if(computerOption=='Rock'):
+                winner='You Won'
+                userPoints+=1
+            else:
+                winner='Computer Won'
+                computerPoints+=1
+        else:
+            if(computerOption=='Rock'):
+                winner='Computer Won'
+                computerPoints+=1
+            else:
+                winner='You Won'
+                userPoints+=1
+    return winner
 
-    def snakesGame():
-        import curses
-        from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
-        from random import randint
+def snakeWaterGun(userOption, computerOption):
+    global userPoints, computerPoints
+    if(userOption==computerOption):
+        winner='Match Drawn'
+    else:
+        if(userOption=='Snake'):
+            if(computerOption=='Water'):
+                winner='You Won'
+                userPoints+=1
+            else:
+                winner='Computer Won'
+                computerPoints+=1
+        elif(userOption=='Water'):
+            if(computerOption=='Snake'):
+                winner='Computer Won'
+                computerPoints+=1
+            else:
+                winner='You Won'
+                userPoints+=1
+        else:
+            if(computerOption=='Snake'):
+                winner='You Won'
+                userPoints+=1
+            else:
+                winner='Computer Won'
+                computerPoints+=1
+    return winner
+
+def invokeSnakeWaterGun():
+    import random
+    numberOfIterations=int(input("Enter the Number of Times you want to play: "))
+    iterationNumber=0
+    userPoints, computerPoints = 0,0
+    nameList = ['Snake','Water','Gun']
+    while(iterationNumber<numberOfIterations):
+        userOption = int(input('Enter 1 for Snake, 2 for Water, 3 for Gun: '))
+        if(userOption==1):
+            userOption='Snake'
+        elif(userOption==2):
+            userOption='Water'
+        else:
+            userOption='Gun'
+        computerOption=random.choice(nameList)
+        winner=snakeWaterGun(userOption, computerOption)
+        print(f"You chose {userOption}\nComputer chose {computerOption}\n{winner} this battle\n\n")
+        print(f"Your Points till now: {userPoints}\nComputer Points till now: {computerPoints}")
+        iterationNumber+=1
+    if(userPoints>computerPoints):
+        print("\n\nYou won the game")
+    elif(computerOption>userPoints):
+        print("\n\nComputer won the game")
+    else:
+        print("\n\nMatch Drawn")
+
+def invokeRockPaperScissor():
+    import random
+    numberOfIterations=int(input("Enter the Number of Times you want to play: "))
+    iterationNumber=0
+    userPoints, computerPoints = 0,0
+    nameList = ['Rock','Paper','Scissor']
+    while(iterationNumber<numberOfIterations):
+        userOption = int(input('Enter 1 for Rock, 2 for Paper, 3 for Scissor: '))
+        if(userOption==1):
+            userOption='Rock'
+        elif(userOption==2):
+            userOption='Paper'
+        else:
+            userOption='Scissor'
+        computerOption=random.choice(nameList)
+        winner=rockPaperScissor(userOption, computerOption)
+        print(f"You chose {userOption}\nComputer chose {computerOption}\n{winner}\n\n")
+        print(f"Your Points till now: {userPoints}\nComputer Points till now: {computerPoints}")
+        iterationNumber+=1
+    if(userPoints>computerPoints):
+        print("\n\nYou won the game")
+    elif(computerOption>userPoints):
+        print("\n\nComputer won the game")
+    else:
+        print("\n\nMatch Drawn")
+    
+def snakesGame():
+    import curses
+    from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+    from random import randint
 
 
-        curses.initscr()
-        win = curses.newwin(20, 60, 0, 0)
-        win.keypad(1)
-        curses.noecho()
-        curses.curs_set(0)
+    curses.initscr()
+    # Initializing the curses. 
+    win = curses.newwin(20, 60, 0, 0)
+    
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
+
+    key = KEY_RIGHT                                                    # Initializing values
+    score = 0
+
+    snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
+    food = [10,20]                                                     # First food co-ordinates
+
+    win.addch(food[0], food[1], '*')                                   # Prints the food
+
+    while key != 27:                                                   # While Esc key is not pressed
         win.border(0)
-        win.nodelay(1)
-
-        key = KEY_RIGHT                                                    # Initializing values
-        score = 0
-
-        snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
-        food = [10,20]                                                     # First food co-ordinates
-
-        win.addch(food[0], food[1], '*')                                   # Prints the food
-
-        while key != 27:                                                   # While Esc key is not pressed
-            win.border(0)
-            win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-            win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-            win.timeout(int(150 - (len(snake)/5 + len(snake)/10)%120))          # Increases the speed of Snake as its length increases
-            
-            prevKey = key                                                  # Previous key pressed
-            event = win.getch()
-            key = key if event == -1 else event 
+        win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
+        win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
+        win.timeout(int(150 - (len(snake)/5 + len(snake)/10)%120))          # Increases the speed of Snake as its length increases
+        
+        prevKey = key                                                  # Previous key pressed
+        event = win.getch()
+        key = key if event == -1 else event 
 
 
-            if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-                key = -1                                                   # one (Pause/Resume)
-                while key != ord(' '):
-                    key = win.getch()
-                key = prevKey
-                continue
+        if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
+            key = -1                                                   # one (Pause/Resume)
+            while key != ord(' '):
+                key = win.getch()
+            key = prevKey
+            continue
 
-            if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
-                key = prevKey
+        if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+            key = prevKey
 
-            # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
-            # This is taken care of later at [1].
-            snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+        # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
+        # This is taken care of later at [1].
+        snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
 
-            # If snake crosses the boundaries, make it enter from the other side
-            if snake[0][0] == 0: snake[0][0] = 18
-            if snake[0][1] == 0: snake[0][1] = 58
-            if snake[0][0] == 19: snake[0][0] = 1
-            if snake[0][1] == 59: snake[0][1] = 1
+        # If snake crosses the boundaries, make it enter from the other side
+        if snake[0][0] == 0: snake[0][0] = 18
+        if snake[0][1] == 0: snake[0][1] = 58
+        if snake[0][0] == 19: snake[0][0] = 1
+        if snake[0][1] == 59: snake[0][1] = 1
 
-            # Exit if snake crosses the boundaries (Uncomment to enable)
-            #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
+        # Exit if snake crosses the boundaries (Uncomment to enable)
+        #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
 
-            # If snake runs over itself
-            if snake[0] in snake[1:]: break
+        # If snake runs over itself
+        if snake[0] in snake[1:]: break
 
-            
-            if snake[0] == food:                                            # When snake eats the food
-                food = []
-                score += 1
-                while food == []:
-                    food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
-                    if food in snake: food = []
-                win.addch(food[0], food[1], '*')
-            else:    
-                last = snake.pop()                                          # [1] If it does not eat the food, length decreases
-                win.addch(last[0], last[1], ' ')
-            win.addch(snake[0][0], snake[0][1], '#')
-            
-        curses.endwin()
-        print("\nScore - " + str(score))
+        
+        if snake[0] == food:                                            # When snake eats the food
+            food = []
+            score += 1
+            while food == []:
+                food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
+                if food in snake: food = []
+            win.addch(food[0], food[1], '*')
+        else:    
+            last = snake.pop()                                          # [1] If it does not eat the food, length decreases
+            win.addch(last[0], last[1], ' ')
+        win.addch(snake[0][0], snake[0][1], '#')
+        
+    curses.endwin()
+    print("\nScore - " + str(score))
 
